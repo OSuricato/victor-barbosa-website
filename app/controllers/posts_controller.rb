34fa3794a43fd647ae8d8246356efda7d8557c2b
victor barbosa <barbosavictor30@gmail.com>
2023-09-21 12:51:54 -0300

    upgrades to navbar responsive

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+    # GET /posts
+    # GET /posts.json
+    def index
+      if params[:category]
+        @posts = Post.where(category: params[:category])
+      else
+        @posts = Post.all
+      end
+    end
+
+    # Other controller actions...
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_post
+        @post = Post.find(params[:id])
+      end
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def post_params
+        params.require(:post).permit(:title, :content, :category)
+      end
 end
